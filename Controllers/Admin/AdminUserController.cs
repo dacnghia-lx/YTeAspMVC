0990e09c720f88721b065a57cd7331ce46710451
nghia-lx <orange25k5@gmail.com>
2026-05-27 01:13:19 +0700

    update admin & doctor admin panel look

--- Controllers/Admin/AdminUserController.cs
+++ Controllers/Admin/AdminUserController.cs
@@ -11,12 +11,39 @@ namespace YTeAspMVC.Controllers.Admin
     public class AdminUserController : Controller
     {
         UserDao userDao = new UserDao();
+        
         // GET: AdminUser
         public ActionResult Index(string msg)
         {
             ViewBag.Msg = msg;
             ViewBag.List = userDao.getUser();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(User user)
+        {
+            if (userDao.checkExistEmail(user.Email))
+            {
+                return RedirectToAction("Index", new { msg = "2" });
+            }
+            user.IdRole = 1;
+            userDao.Add(user);
+            return RedirectToAction("Index", new { msg = "1" });
+        }
+
+        [HttpPost]
+        public ActionResult Update(User user)
+        {
+            userDao.Update(user);
+            return RedirectToAction("Index", new { msg = "1" });
+        }
+
+        [HttpPost]
+        public ActionResult Delete(User user)
+        {
+            userDao.Delete(user.IdUser);
+            return RedirectToAction("Index", new { msg = "1" });
         }
     }
 }
